e16b6d22d82534f964ae52f297a54a7dcd329b1c
Evgeny Pestov <pestov_ev@bw-sw.com>
2016-03-20 23:08:20 +0700

    --mode=child is complete.

--- src/child.c
+++ src/child.c
@@ -9,15 +9,13 @@
 
 void handle_child(int signal, siginfo_t *siginfo, void *context) {
     if (SIGCHLD == signal) {
-        printf("SIGCHLD handling\n");
-        printf("Child PID: %i\n", siginfo->si_pid);
-        printf("User of process: %i\n", siginfo->si_uid);
-        printf("Status: %i\n", siginfo->si_status);
-        printf("Code: %i\n", siginfo->si_code);
-        exit(0);
+        printf("Handler: SIGCHLD catched.\n");
+        printf("Handler: si_pid=%i\n", siginfo->si_pid);
+        printf("Handler: si_uid=%i\n", siginfo->si_uid);
+        printf("Handler: si_status=%i\n", siginfo->si_status);
+        printf("Handler: si_code=%i\n", siginfo->si_code);
     } else {
-        perror("unexpected signal\n");
-        exit(11);
+        perror("Handler: unexpected signal.\n");
     }
 }
 
@@ -25,26 +23,22 @@ void script_child() {
     struct sigaction sa;
     sa.sa_flags = SA_SIGINFO;
     sa.sa_sigaction = &handle_child;
-
     int sleep_time = 5;
-
     pid_t pid = fork();
     if (0 == pid) {  // child
-        printf("sleep %d seconds.\n", sleep_time);
+        printf("Child:   started.\n");
+        printf("Child:   sleep %d seconds.\n", sleep_time);
         sleep(sleep_time);
-        printf("I woke up.\n");
+        printf("Child:   I woke up.\n");
+        printf("Child:   finished.\n");
     } else if (pid > 0) {  // parent
+        printf("Parent:  started.\n");
         sigaction(SIGCHLD, &sa, NULL);
         int status;
-        if (wait(&status) > 0) {
-            printf("S\n");
-            exit(0);
-        } else {
-            perror("Failed to handle CHLD zombie\n");
-            exit(12);
-        }
+        wait(&status);
+        printf("Parent:  finished.\n");
     } else {  // (pid < 0) error
-        perror("pid is negative, after fork.\n");
+        perror("?:       pid is negative, after fork.\n");
         exit(10);
     }
 }
